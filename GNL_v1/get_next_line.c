4dd4af0ec73750394fb85ae16f79f63fc0b7132d
Abbes Dakhama Bennani <adakhama@2B1.42angouleme.fr>
2025-11-14 16:21:22 +0100

    version guidee garce aux coms

--- GNL_v1/get_next_line.c
+++ GNL_v1/get_next_line.c
@@ -6,7 +6,17 @@
 /*   By: adakhama <adakhama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 14:15:38 by adakhama          #+#    #+#             */
-/*   Updated: 2025/11/14 14:15:39 by adakhama         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:17:57 by adakhama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char *get_next_line(int fd)
+{
+    
+}
+
+/*
+ft get next line : remplit les buffer grace a calloc et appele (ft get line);
+ft get line : utilise strjoin pour concatener le buffer remplis et la dest qui ne range pas entre chaque appel, elle n'utilise que 3 char*;
+ft read : remplis le buffer et utilise strchr pour verifier la presence de '\n' et affecte une valeur a bn en fonction de la veleur du strchr; 
+*/
